2189bcf7802aa2e2846abaebf5fc6b0d497904ee
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-31 14:30:25 +1200

    Write actions functions

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,3 +1,19 @@
-import {} from '../apis/jobs'
+import { getJobs } from '../apis/jobs'
 
-export const SET_FRUITS = 'SET_FRUITS'
+export const SET_JOBS = 'SET_JOBS'
+
+export function setJobs(jobs) {
+  return {
+    type: SET_JOBS,
+    payload: jobs,
+  }
+}
+
+export function fetchJobs() {
+  return (dispatch) => {
+    return getJobs().then((jobs) => {
+      dispatch(setJobs(jobs))
+      return null
+    })
+  }
+}
