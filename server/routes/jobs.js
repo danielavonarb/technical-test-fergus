515470898620346f0a71e9db268c46578d6180b6
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-09-06 14:09:04 +1200

    Create routes

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,6 +4,29 @@ const db = require('../db/jobs')
 
 const router = express.Router()
 
-// TODO
+router.get('/', (req, res) => {
+  db.getJobs()
+    .then((results) => {
+      res.json(results)
+      return null
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Somthing went wrong' })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  db.getJob(id)
+    .then((result) => {
+      res.json(result)
+      return null
+    })
+    .catch((err) => {
+      console.error(err.message)
+      res.status(500).json({ message: 'Somthing went wrong' })
+    })
+})
 
 module.exports = router
